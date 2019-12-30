0c961b58cbbac416a04b69bdba59e19da4680f33
henryho2012 <henryho2012@gmail.com>
2019-12-30 22:23:04 +0800

    Update pipelines.py

--- homework/Day027_HW/Day027_HW/pipelines.py
+++ homework/Day027_HW/Day027_HW/pipelines.py
@@ -5,7 +5,16 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import json
 
-class Day026HwPipeline(object):
+class Day027HwPipeline(object):
     def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
         return item
+    def open_spider(self, spider):
+        print('Start!!')
+        self.file = open('Day27_HW.txt', 'w')
+    def close_spider(self, spider):
+        self.file.close()
+        print('Finished!!')
